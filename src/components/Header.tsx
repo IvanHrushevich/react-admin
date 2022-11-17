6e0141a6d76d414deb7939180179ad87e6e4a9c6
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-11-17 04:13:13 +0300

    create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { FC } from "react";
+
+import { tokens } from "../theme";
+
+interface Props {
+  title: string;
+  subtitle: string;
+}
+
+const Header: FC<Props> = ({ title, subtitle }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ m: "0 0 5px 0" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" color={colors.greenAccent[400]}>
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,7 +1,16 @@
+import { Box } from "@mui/material";
 import { FC } from "react";
 
+import Header from "../../components/Header";
+
 const Dashboard: FC = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="WELCOME TO DASHBOARD" />
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
