64dfd0adf743e6f1543235a20d50ee043f1538e3
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-11-21 15:24:52 +0300

    implement ProgressCircle and StatBox components, slightly update Dashboard page

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,13 +1,32 @@
-import { Box } from "@mui/material";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import { Box, Button, useTheme } from "@mui/material";
 import { FC } from "react";
 
 import Header from "../../components/Header";
+import { tokens } from "../../theme";
 
 const Dashboard: FC = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="WELCOME TO DASHBOARD" />
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
